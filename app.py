b0f6e822e6bf11413c28931adf047476003d4a3b
karlo7rdtz <karlo.rdgzt20707@gmail.com>
2026-05-22 00:23:04 -0600

    no hice nada jsjs

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 # Permitir que el frontend se comunique con el backend
 CORS(app)
 
-# Configuración de la base de datos Neon (Intenta leerla desde Azure, si no, usa el string por defecto)
 DATABASE_URL = os.environ.get(
     'DATABASE_URL', 
     "postgresql://neondb_owner:npg_oQ4BrhMS9WEi@ep-icy-bonus-ap3ijfeu-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
